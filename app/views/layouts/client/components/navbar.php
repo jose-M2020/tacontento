ea26ca34e8567601f3791fb64b6427b6a5278b10
Jose Manuel <josemanuel.silva.jmst@gmail.com>
2023-09-03 23:39:13 -0600

    add client layout

---
+++ app/views/layouts/client/components/navbar.php
@@ -0,0 +1,50 @@
+<?php function createNavbar($scrollEffect = 'true') { ?>
+    <header data-scroll-effect="<?php echo $scrollEffect ?>">
+        <div class="container d-flex justify-content-between">
+            <div class="d-flex align-items-center">
+              <a class="logo d-flex align-items-center" href="<?= BASE_URL ?>/home">
+                <img src="images/taco.png" alt="Logo">
+                <h1 class="logo-name">Ta'contento</h1>
+              </a>
+            </div>
+            <button class="faded-text menu-nav-icon" data-menu="#main-menu">
+              <i class="ion-navicon"></i>
+            </button>
+            
+
+            <ul class="main-menu" id="main-menu">
+              <li><a class="faded-text" href="<?= BASE_URL ?>/home">INICIO</a></li>
+              <li><a class="faded-text" href="<?= BASE_URL ?>/menu">MENU</a></li>
+              <li><a class="faded-text" href="<?= BASE_URL ?>/services">SERVICIOS</a></li>
+              <li><a class="faded-text" href="<?= BASE_URL ?>/about">ACERCA</a></li>
+              <?php if (isset($_SESSION['usuario']) && ($_SESSION['usuario']['role'] === 'client') ) : ?>
+                <li>
+                  <a class="faded-text position-relative" href="<?= BASE_URL ?>/carrito">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                    <span class="bg-primary position-absolute top-0 start-100 translate-middle badge border border-light rounded-pill d-flex justify-content-center align-items-center p-2" style="width: 20px; height:20px;">
+                      <?php echo($_SESSION['usuario']['cartNum']) ?>
+                    </span>
+                  </a>
+                </li>                  
+                <div class="nav-item dropdown">
+                  <a href="#" class="nav-link dropdown-toggle faded-text" data-bs-toggle="dropdown">
+                      <i class="fa-solid fa-circle-user"></i>
+                      Cuenta
+                  </a>
+                  <div class="dropdown-menu account dropdown-menu-end m-0">
+                      <a class="dropdown-item faded-text" href="<?= BASE_URL ?>/createreserva"><i class="me-3 fa-solid fa-table-list"></i>RESERVAS</a>
+                      <a class="dropdown-item faded-text" href="<?= BASE_URL ?>/compras"><i class="me-3 fa-solid fa-utensils"></i>COMPRAS</a>
+                      <hr class="dropdown-divider">
+                      <a class="dropdown-item faded-text" href="<?= BASE_URL ?>/logout"><i class="me-3 fa-solid fa-right-from-bracket"></i>Cerrar sesión</a>
+                  </div>
+                </div>
+              <?php else : ?>
+                  <li>
+                    <a class="btn btn-primary faded-text" href="<?= BASE_URL ?>/auth">Entrar</a>
+                  </li>
+              <?php endif; ?>
+              <!-- <button class="btn btn-sm btn-primary" type="button">Ordenar</button> -->
+            </ul>
+        </div><!-- container -->
+    </header>
+<?php } ?>
---
+++ app/views/layouts/client/index.php
@@ -0,0 +1,113 @@
+<?php
+require_once 'app/config/config.php';
+
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+
+$navItems = [
+  0 => [
+    "name" => 'INICIO',
+    "link" => '<?= BASE_URL ?>/home',
+    "checkLogin" => false,
+  ],
+  1 => [
+    "name" => 'MENU',
+    "link" => '<?= BASE_URL ?>/menu',
+    "checkLogin" => false,
+  ],
+  2 => [
+    "name" => 'SERVICIOS',
+    "link" => '<?= BASE_URL ?>/services',
+    "checkLogin" => false,
+  ],
+  3 => [
+    "name" => 'ACERCA',
+    "link" => '<?= BASE_URL ?>/about',
+    "checkLogin" => false,
+  ],
+  4 => [
+    "icon" => 'fa-solid fa-cart-shopping',
+    "link" => '<?= BASE_URL ?>/carrito',
+    "checkLogin" => true,
+  ],
+  5 => [
+    "name" => "CUENTA",
+    "icon" => 'fa-solid fa-cart-shopping',
+    "link" => '#',
+    "checkLogin" => true,
+    "children" => [
+      0 => [
+        "name" => 'RESERVAS',
+        "ICON" => "fa-solid fa-table-list",
+        "link" => '<?= BASE_URL ?>/createreserva',
+        "checkLogin" => true,
+      ],
+      1 => [
+        "name" => 'COMPRAS',
+        "ICON" => "fa-solid fa-utensils",
+        "link" => '<?= BASE_URL ?>/compras',
+        "checkLogin" => true,
+      ],
+      2 => [
+        "name" => 'CERRAR SESIÓN',
+        "ICON" => "fa-solid fa-right-from-bracket",
+        "link" => '<?= BASE_URL ?>/logout',
+        "checkLogin" => true,
+      ],
+    ]
+  ],
+];
+
+$title = $this->getSection('title') ? ($this->getSection('title') . ' - ') : '';
+
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    <title>
+      <?= $title ?>Ta'Contento
+    </title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="images/taco.png">
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Concert+One&family=Dosis:wght@400;600;800&display=swap" rel="stylesheet">
+    <!-- Libraries Stylesheet -->
+    <link href="public/plugin-frameworks/animate/animate.min.css" rel="stylesheet">
+    <!-- Stylesheets -->
+    <link href="public/plugin-frameworks/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <link href="public/plugin-frameworks/swiper.css" rel="stylesheet">
+    <link href="public/fonts/ionicons.css" rel="stylesheet">
+    <link href="public/common/css/styles.css" rel="stylesheet">
+    <!-- <link href="public/fonts/all.min.css" rel="stylesheet"> -->
+    
+    <!-- CDN font-awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="public/common/all.min.js"></script>
+
+</head>
+
+<body>
+  <?php 
+    include_once "components/navbar.php";
+    require_once('./app/views/components/message.php');
+
+    echo createNavbar($this->params['scrollEffect'] ?? 'true');
+  ?>
+
+  <main class="">
+    <?= $this->getSection('content'); ?>
+  </main>
+
+  <?php 
+    include_once "components/footer.php";
+    echo($this->getSection('scripts'));
+  ?>
+</body>
+</html>
